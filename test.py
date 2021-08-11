bfd1cf979054ea2047b0f89b6bc9136f747ca0a3
Arthur-r19 <arl@ic.ufal.br>
2021-08-11 03:47:44 -0300

    COISA P CARAI

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-import PySimpleGUI as sg
+import re
 
 
-layout = [[
-    sg.Frame('Dados Pessoais', 
-    [[sg.Column([[sg.Text('Nome')],[sg.Text('Endereço')]]), 
-    sg.Column([[sg.Input(size=(30,1), key='MAE-NOME')],[sg.Input(size=(30,1), key='MAE-ENDERECO')]])]]
-    )
-]]
+str1 = '111122aaaa2323'
 
 
-sg.Window('test', layout, finalize=True)
-
-
-
-
-
-
-[
-                                            [,
-                                            ], 
-                                            [, ],
-                                            [, ]
-                                            ]
+print()
